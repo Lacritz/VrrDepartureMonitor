3970f4e0b76a7ecbcc9b515ff6082072ae209604
Gorden Wunderlich <Gorden.Wunderlich@uni-duesseldorf.de>
2018-03-05 13:02:43 +0100

    fixed caching

--- src/main/java/message/urlreader/URLReader.java
+++ src/main/java/message/urlreader/URLReader.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 
 
 public class URLReader {
@@ -14,8 +15,17 @@ public class URLReader {
     }
 
 
-    public static String read(URL url) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+    public static String read(URL url) throws Exception {
+    	URLConnection connection = url.openConnection();
+    	connection.setConnectTimeout(2000);
+    	connection.setReadTimeout(2000);
+        connection.setUseCaches(false);
+        connection.setDefaultUseCaches(false);
+        connection.setRequestProperty("Cache-Control", "no-cache, no-store, must-revalidate");
+        connection.setRequestProperty("Pragma", "no-cache");
+        connection.setRequestProperty("Expires", "0");
+        connection.connect();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String message = "";
         message = in.lines().reduce("", String::concat);
         in.close();
